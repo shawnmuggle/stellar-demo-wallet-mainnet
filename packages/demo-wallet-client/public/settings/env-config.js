dfc9ab2106626992a3ceba116f14e446c7c9148e
Shawn Muggle <shawn@mugglepay.com>
2026-04-03 21:11:47 +0800

    Update env-config.js

--- packages/demo-wallet-client/public/settings/env-config.js
+++ packages/demo-wallet-client/public/settings/env-config.js
@@ -1,8 +1,8 @@
 window._env_ = {
   AMPLITUDE_API_KEY: "",
   SENTRY_API_KEY: "",
-  HORIZON_PASSPHRASE: "",
-  HORIZON_URL: "",
+  HORIZON_PASSPHRASE: "Public Global Stellar Network ; September 2015",
+  HORIZON_URL: "https://horizon.stellar.org",
   RPC_PASSPHRASE: "",
   RPC_URL: "",
   CLIENT_DOMAIN: "",
